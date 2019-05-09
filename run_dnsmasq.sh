210aec9c5813cb53ffee95c9dcf53276d762efdf
atyronesmith <aasmith@redhat.com>
2019-05-09 07:58:01 -0400

    first working version

--- run_dnsmasq.sh
+++ run_dnsmasq.sh
@@ -2,10 +2,11 @@
 set -x
 
 DNSMASQ_CONF_DIR="/var/run/dnsmasq-bm"
+DNSMASQ_CONF_FILE="${DNSMASQ_CONF_DIR}/dnsmasq-bm.conf"
 DNSMASQ_PID_FILE="${DNSMASQ_CONF_DIR}/dnsmasq-bm.pid"
 
 cleanup() {
-  echo "$? cleanup $1"
+    echo "$? cleanup $1"
 }
 
 trap cleanup EXIT
@@ -15,7 +16,7 @@ usage() {
     Starts/stops/installs a dnsmasq instance to serve DNS/DHCP for the baremetal network in the METAL3 environment.
     Usage:
     $(basename $0) start config_file|stop
-    Starts a dnsmasq instance to serve DNS and DHCP for the baremetal network in the metal3 environment. 
+    Starts a dnsmasq instance to serve DNS and DHCP for the baremetal network in the metal3 environment.
         start config_file -- Start the dnsmasq instance with the variables define in the config_file.
                               The config_file is the file that is passed to metal3 deploy.
                               i.e.
@@ -60,17 +61,17 @@ fi
 
 stop_dnsmasq()
 {
-  if [ -e "$DNSMASQ_PID_FILE" ]; then
-     kill $(cat ${DNSMASQ_PID_FILE})
-     if [ $? -ne 0 ]; then
-       echo "Could not stop dnsmasq instance, must stop manually..."
-       exit 1
-     fi
-     exit 0
-  else
-     echo "No pid file, $DNSMASQ_PID_FILE, not running or must stop manually..."
-     exit 1
-  fi
+    if [ -e "$DNSMASQ_PID_FILE" ]; then
+        kill $(cat ${DNSMASQ_PID_FILE})
+        if [ $? -ne 0 ]; then
+            echo "Could not stop dnsmasq instance, must stop manually..."
+            exit 1
+        fi
+        exit 0
+    else
+        echo "No pid file, $DNSMASQ_PID_FILE, not running or must stop manually..."
+        exit 1
+    fi
 }
 
 check_var()
@@ -80,40 +81,53 @@ check_var()
     if [ -z "${!varname}" ]; then
         echo "$varname not set in ${config_file}, must define $varname"
         exit 1
-    fi        
+    fi
+}
+
+read_config()
+{
+    config_file=$1
+    
+    if [ -z ${config_file+defined} ]; then
+        echo "Missing config file arg..."
+        usage
+        exit 1
+    fi
+    
+    if [ ! -e "${config_file}" ]; then
+        echo "Config file: ${config_file}, does not exist..."
+        exit 1
+    fi
+    
+    source "${config_file}"
+    
+    check_var BASE_DOMAIN ${config_file}
+    check_var CLUSTER_NAME ${config_file}
+    check_var INT_IF ${config_file}
+}
+
+stop_dnsmasq()
+{
+    if [ -e ${DNSMASQ_PID_FILE} ]; then
+        pid=$(cat ${DNSMASQ_PID_FILE})
+        sudo kill $pid
+    fi
 }
 
 start_dnsmasq()
 {
-  config_file=$1
-       
-  if [ ! -e "${config_file}" ]; then
-     echo "Config file: ${config_file}, does not exist..."
-     exit 1
-  fi
+    stop_dnsmasq
+    sudo dnsmasq -x ${DNSMASQ_PID_FILE} -q -C ${DNSMASQ_CONF_FILE}
+}
 
-  source "${config_file}"
-
-  check_var BASE_DOMAIN ${config_file}
-  check_var CLUSTER_NAME ${config_file}
-  check_var INT_IF ${config_file}
-
-  if [ -e ${DNSMASQ_PID_FILE} ]; then
-    pid=$(cat ${DNSMASQ_PID_FILE})
-    kill -0 $pid
-    if [ $? -ne 0 ]; then
-      rm -f ${DNSMASQ_PID_FILE}
-    else
-      echo "baremetal dnsmasq instance is already running as PID $pid..."
-      exit 1
-    fi
-  fi
-
-  mkdir -p ${DNSMASQ_CONF_DIR}
-
-  cat << EOF > ${DNSMASQ_CONF_DIR}/dnsmasq-bm.conf
+create_dnsmasq_conf()
+{
+    sudo mkdir -p ${DNSMASQ_CONF_DIR}
+    
+    {
+  cat << EOF
 strict-order
-server=/apps.${CLUSTER_NAME}.${BASE_DOMAIN}/127.0.0.1 
+server=/apps.${CLUSTER_NAME}.${BASE_DOMAIN}/127.0.0.1
 local=/${CLUSTER_NAME}.${BASE_DOMAIN}/
 domain=${CLUSTER_NAME}.${BASE_DOMAIN}
 expand-hosts
@@ -128,55 +142,55 @@ dhcp-lease-max=41
 dhcp-hostsfile=${DNSMASQ_CONF_DIR}/baremetal.hostsfile
 addn-hosts=${DNSMASQ_CONF_DIR}/baremetal.addnhosts
 EOF
-
+    } | sudo dd of=${DNSMASQ_CONF_FILE}
+    
 }
 
 setup_bridge()
 {
     bridge=$1
     intf=$2
+    ip_address=$3
+    ip_netmask=$4
 
-    if [ -e /etc/sysconfig/network-scripts/ifcfg-${bridge} ] ; then
-        echo "Bridge ${bridge} already define!  Undefine/remove before running.."
+    if [ -z ${bridge} ]; then
+        echo "Missing bridge arg..."
         exit 1
-        echo -e "DEVICE=baremetal\nTYPE=Bridge\nONBOOT=yes\nNM_CONTROLLED=no" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-baremetal
     fi
-    sudo ifdown baremetal || true
-    sudo ifup baremetal
-
-    # Add the internal interface to it if requests, this may also be the interface providing
-    # external access so we need to make sure we maintain dhcp config if its available
-    if [ "$INT_IF" ]; then
-        echo -e "DEVICE=$INT_IF\nTYPE=Ethernet\nONBOOT=yes\nNM_CONTROLLED=no\nBRIDGE=baremetal" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-$INT_IF
-        if sudo nmap --script broadcast-dhcp-discover -e $INT_IF | grep "IP Offered" ; then
-            echo -e "\nBOOTPROTO=dhcp\n" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-baremetal
-            sudo systemctl restart network
-        else
-           sudo systemctl restart network
-        fi
+    
+    if [ -z ${intf} ]; then
+        echo "Missing interface arg..."
+        exit 1
     fi
+    
+    echo -e "DEVICE=${bridge}\nTYPE=Bridge\nONBOOT=yes\nNM_CONTROLLED=no\nIPADDR=${ip_address}\nNETMASK=${ip_netmask}" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-${bridge}
+    
+    sudo ifdown ${bridge} || true
+    sudo ifup ${bridge}
+    
+    echo -e "DEVICE=${intf}\nTYPE=Ethernet\nONBOOT=yes\nNM_CONTROLLED=no\nBRIDGE=${bridge}" | sudo dd of=/etc/sysconfig/network-scripts/ifcfg-${intf}
 
+    sudo systemctl restart network
 }
 
 COMMAND=$1
 
 case "$COMMAND" in
-     start)
-       if [ -z ${2+defined} ]; then
-          echo "Missing config file arg..."
-          usage
-       fi
-       start_dnsmasq "$2"
-       ;;
-     stop)
-       stop_dnsmasq
-       ;;
-     install)
-       ;;
-     *)
-       echo "Unknown command: ${COMMAND}"
-       usage
-       ;;
+    start)
+        read_config $2
+        create_dnsmasq_conf
+        setup_bridge "baremetal" $INT_IF "192.168.111.1" "255.255.255.0"
+        start_dnsmasq
+    ;;
+    stop)
+        stop_dnsmasq
+    ;;
+    install)
+    ;;
+    *)
+        echo "Unknown command: ${COMMAND}"
+        usage
+    ;;
 esac
 
 
